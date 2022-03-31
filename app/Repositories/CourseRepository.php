f7640febf1f46f1e9f90a12fb169f90e90f3cdc4
Jorge Henrique Jesus da SIlva <rick@MacBook-Pro-de-Jorge.local>
2022-03-31 19:09:19 -0300

    feito os endpoints de listagem de curso e cursos

---
+++ app/Repositories/CourseRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Api\Course;
+
+class CourseRepository
+{
+    protected $entity;
+
+    public function __construct(Course $model)
+    {
+        $this->entity = $model;
+    }
+
+    public function getAllCourses()
+    {
+        return $this->entity->get();
+    }
+    public function getCourse($id)
+    {
+        return $this->entity->findOrFail($id);
+    }
+}
---
+++ database/factories/Api/CourseFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories\Api;
+
+use App\Models\Api\Course;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class CourseFactory extends Factory
+{
+    protected $model = Course::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->unique()->name(),
+            'description' => $this->faker->sentence(20)
+        ];
+    }
+}
